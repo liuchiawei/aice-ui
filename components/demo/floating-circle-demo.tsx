90790628248c270c5ce831b4a9fc16bde4b1ee3f
LIU CHIAWEI <doublecheap@gmail.com>
2026-02-17 16:58:02 +0900

    style(floating-circle): enlarge demo height

--- components/demo/floating-circle-demo.tsx
+++ components/demo/floating-circle-demo.tsx
@@ -2,13 +2,13 @@ import { FloatingCircles } from "@/components/background/floating-circles";
 
 export function FloatingCirclesDemo() {
   return (
-    <div className="relative h-48 w-full max-w-md overflow-hidden rounded-lg border border-border">
+    <div className="relative h-108 w-full overflow-hidden rounded-lg border border-border">
       <FloatingCircles
         className="absolute inset-0"
-        size="60%"
+        size="20%"
         interactive={true}
       />
-      <div className="relative flex h-full items-center justify-center">
+      <div className="relative flex h-full items-center justify-center z-10">
         <span className="text-sm font-medium text-foreground/90">
           Gradient background
         </span>
@@ -17,7 +17,7 @@ export function FloatingCirclesDemo() {
   );
 }
 
-export const floatingCirclesSource = `<div className="relative h-48 overflow-hidden rounded-lg border">
+export const floatingCirclesSource = `<div className="relative h-108 overflow-hidden rounded-lg border">
   <FloatingCircles
     className="absolute inset-0"
     size="60%"
