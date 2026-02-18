42a926ca6ea19530a1994e7110150c8dbec89610
LIU CHIAWEI <doublecheap@gmail.com>
2026-02-18 17:43:38 +0900

    feat(locale): add generateStaticParams function for locale routing; remove unused dashboard page

--- app/[locale]/components/page.tsx
+++ app/[locale]/components/page.tsx
@@ -1,9 +1,14 @@
 import { setRequestLocale } from "next-intl/server";
+import { routing } from "@/i18n/routing";
 import { ComponentsPageClient } from "./components-page-client";
 
 type Props = {
   params: Promise<{ locale: string }>;
 };
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
 
 export default async function ComponentsPage({ params }: Props) {
   const { locale } = await params;
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,14 @@
 import { setRequestLocale } from "next-intl/server";
+import { routing } from "@/i18n/routing";
 import Hero from "@/components/section/hero";
 
 type Props = {
   params: Promise<{ locale: string }>;
 };
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
 
 export default async function Home({ params }: Props) {
   const { locale } = await params;
