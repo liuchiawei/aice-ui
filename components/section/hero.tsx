caeeff9ca2c24ed2ecaf524824e239ba25cc03e1
LIU CHIAWEI <doublecheap@gmail.com>
2026-02-18 17:48:09 +0900

    feat(i18n): update home page messages

--- components/section/hero.tsx
+++ components/section/hero.tsx
@@ -1,16 +1,17 @@
 import { Link } from "@/i18n/navigation";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
-import { topPage } from "@/lib/message";
+import { useTranslations } from "next-intl";
 
 export default function Hero() {
+  const t = useTranslations("Home");
   return (
     <main className="mx-auto flex max-w-xl flex-col items-center gap-10 text-center">
       <h1 className="text-7xl md:text-[10vw] font-roboto font-black tracking-tight text-foreground uppercase leading-none">
-        {topPage.title}
+        {t("title")}
       </h1>
       <p className="w-full max-w-md text-sm text-muted-foreground text-justify">
-        {topPage.description}
+        {t("description")}
       </p>
       <Button asChild>
         <Link
@@ -18,7 +19,7 @@ export default function Hero() {
           className="group inline-flex items-center gap-2"
         >
           <ArrowRight className="size-4 group-hover:translate-x-1 transition-transform duration-300" />{" "}
-          {topPage.CTA}
+          {t("CTA")}
         </Link>
       </Button>
     </main>
