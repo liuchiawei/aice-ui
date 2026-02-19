96c9994f5595b929697d95f1017bdd82b8df40cd
LIU CHIAWEI <doublecheap@gmail.com>
2026-02-19 13:28:31 +0900

    feat(glass-surface-demo): update text styling

--- components/demo/glass-surface-demo.tsx
+++ components/demo/glass-surface-demo.tsx
@@ -5,10 +5,10 @@ import { ChevronRight } from "lucide-react";
 
 function GlassSurfaceDemo() {
   return (
-    <div className="h-full w-full p-12 flex justify-center items-center text-white bg-[url('https://picsum.photos/800/600')] bg-cover bg-center">
+    <div className="h-full w-full p-12 flex justify-center items-center text-white bg-[url('https://picsum.photos/800/600')] bg-cover bg-center bg-fixed">
       <GlassSurface width={180} height={60} borderRadius={16} contentClassName="group flex items-center justify-center gap-4">
         <ChevronRight className="size-4 group-hover:translate-x-2 transition-transform duration-300" />
-        Hover me
+        <span className="font-black text-shadow-lg">Hover me</span>
       </GlassSurface>
     </div>
   );
@@ -18,12 +18,10 @@ const glassSurfaceSource = `import GlassSurface from "@/components/ui-elements/g
 import { Sun } from "lucide-react";
 
 return (
-  <div className="h-full w-full p-12 flex justify-center items-center text-white bg-[url('https://picsum.photos/800/600')] bg-cover bg-center">
-    <GlassSurface width={180} height={60} borderRadius={16} contentClassName="group flex items-center justify-center gap-4">
-      <ChevronRight className="size-4 group-hover:translate-x-2 transition-transform duration-300" />
+  <GlassSurface width={180} height={60} borderRadius={16} contentClassName="group flex gap-4">
+    <ChevronRight className="size-4 group-hover:translate-x-2" />
       Hover me
-    </GlassSurface>
-  </div>
+  </GlassSurface>
 );`;
 
 export { GlassSurfaceDemo, glassSurfaceSource };
