7dd2dfb378d532c6992a20f99d606ffae000b89f
LIU CHIAWEI <doublecheap@gmail.com>
2026-02-19 16:59:08 +0900

    fix(glass-surface-demo): update borderRadius for GlassSurface component and improve demo code structure

--- components/demo/glass-surface-demo.tsx
+++ components/demo/glass-surface-demo.tsx
@@ -6,7 +6,12 @@ import { ChevronRight } from "lucide-react";
 function GlassSurfaceDemo() {
   return (
     <div className="h-full w-full p-12 flex justify-center items-center text-white bg-[url('https://picsum.photos/800/600')] bg-cover bg-center bg-fixed">
-      <GlassSurface width={180} height={60} borderRadius={16} contentClassName="group flex items-center justify-center gap-4">
+      <GlassSurface
+        width={180}
+        height={60}
+        borderRadius={30}
+        contentClassName="group flex items-center justify-center gap-4"
+      >
         <ChevronRight className="size-4 group-hover:translate-x-2 transition-transform duration-300" />
         <span className="font-black text-shadow-lg">Hover me</span>
       </GlassSurface>
@@ -17,11 +22,17 @@ function GlassSurfaceDemo() {
 const glassSurfaceSource = `import GlassSurface from "@/components/ui-elements/glass-surface";
 import { Sun } from "lucide-react";
 
-return (
-  <GlassSurface width={180} height={60} borderRadius={16} contentClassName="group flex gap-4">
-    <ChevronRight className="size-4 group-hover:translate-x-2" />
-      Hover me
-  </GlassSurface>
-);`;
+const GlassSurfaceDemo = () => {
+  return (
+    <GlassSurface
+      width={180}
+      height={60}
+      borderRadius={30}
+      contentClassName="group flex gap-4"
+    >
+      <ChevronRight className="size-4 group-hover:translate-x-2" />
+        Hover me
+    </GlassSurface>
+)};`;
 
 export { GlassSurfaceDemo, glassSurfaceSource };
