8369039715debe8ab1232d5c69f43bde0f755fcf
LIU CHIAWEI <doublecheap@gmail.com>
2026-02-20 14:56:05 +0900

    feat(localization): add scroll button translations in English, Japanese, and Chinese; update demo source code

--- components/demo/scroll-button-demo.tsx
+++ components/demo/scroll-button-demo.tsx
@@ -3,15 +3,23 @@ import { ScrollButton } from "@/components/button/scroll-button";
 export function ScrollButtonDemo() {
   return (
     <div className="flex flex-wrap gap-2 justify-center items-center min-h-32">
-      <ScrollButton.Top />
-      <ScrollButton.Bottom />
-      <ScrollButton.Up />
-      <ScrollButton.Down />
+      <ScrollButton.Up tooltip="Up" />
+      <ScrollButton.Down tooltip="Down" />
+      <ScrollButton.Top tooltip="Top" />
+      <ScrollButton.Bottom tooltip="Bottom" />
     </div>
   );
 }
 
 export const scrollButtonSource = `import { ScrollButton } from "@/components/button/scroll-button";
 
-<ScrollButton.Top tooltip="Scroll to top" />
-<ScrollButton.Down tooltip="Scroll down" />`;
+export function ScrollButtonDemo() {
+  return (
+    <div className="flex flex-wrap gap-2 justify-center items-center min-h-32">
+      <ScrollButton.Up tooltip="Up" />
+      <ScrollButton.Down tooltip="Down" />
+      <ScrollButton.Top tooltip="Top" />
+      <ScrollButton.Bottom tooltip="Bottom" />
+    </div>
+  );
+}`;
